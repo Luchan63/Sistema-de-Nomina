b61cc402c2efd012925066c6ebc3bfbedae32ae1
Luis Anibal Figuereo Ali <luisanibalfiguereo@hotmail.com>
2024-01-18 13:36:17 +0100

    primera Version

---
+++ Capitulos1-4/Bucles/src/While.java
@@ -0,0 +1,15 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+public class While
+{
+    public static void main(String[] args) {
+        for(int contador = 10; contador >= 0; --contador) {
+            System.out.println(contador);
+        }
+
+        System.out.println("Despega");
+    }
+}
