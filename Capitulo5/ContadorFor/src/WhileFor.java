4e4e457142c8afd9ff608239f1081e0efdff46c2
Luis Anibal Figuereo Ali <luisanibalfiguereo@hotmail.com>
2024-01-19 11:18:10 +0100

    segunda version

---
+++ Capitulo5/ContadorFor/src/WhileFor.java
@@ -0,0 +1,20 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+public class WhileFor {
+    public WhileFor() {
+    }
+
+    public static void main(String[] args) {
+        int contador = 0;
+
+        for(int limite = 3; contador < limite; ++contador) {
+            for(int i = 1; i <= 3; ++i) {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
